e8b48841dd215855bf1aaad8de4322fa0ee27023
sanjana-04 <66238989+sanjana-04@users.noreply.github.com>
2021-06-22 02:27:41 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ $(function () {
         $(this).parent().find('.input-group-text').css('border-color', '#ced4da');
     });
 });
+
+
+  function doc(){
+      document.getElementById("doc").style.display = "block";
+      document.getElementById("stu").style.display = "none";
+      document.getElementById("other").style.display = "none";
+  }
+  function stu(){
+      document.getElementById("stu").style.display = "block";
+      document.getElementById("doc").style.display = "none";
+      document.getElementById("other").style.display = "none";
+  }
+  function other(){
+      document.getElementById("stu").style.display = "none";
+      document.getElementById("doc").style.display = "none";
+      document.getElementById("other").style.display = "block";
+  }
